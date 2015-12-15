a1da5f534b56ce5292a9b1c78c07129731a1ee9c
freedom <stefano.vettor@mac.com>
2015-12-15 20:40:56 +0100

    Initial Commit

---
+++ PopInAndOutCollectionViewTransition/PopInAndOutCollectionViewTransition.h
@@ -0,0 +1,19 @@
+//
+//  PopInAndOutCollectionViewTransition.h
+//  PopInAndOutCollectionViewTransition
+//
+//  Created by Stefano Vettor on 15/12/15.
+//  Copyright © 2015 Stefano Vettor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopInAndOutCollectionViewTransition.
+FOUNDATION_EXPORT double PopInAndOutCollectionViewTransitionVersionNumber;
+
+//! Project version string for PopInAndOutCollectionViewTransition.
+FOUNDATION_EXPORT const unsigned char PopInAndOutCollectionViewTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopInAndOutCollectionViewTransition/PublicHeader.h>
+
+
